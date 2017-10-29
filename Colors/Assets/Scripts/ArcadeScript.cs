b4d9609d09b5e9c43006476794605fbae38f37fa
DESKTOP-DHF1UVD\moham <mohammad.chowdhry@contax.com>
2017-10-29 00:40:33 -0400

    Initialized repository with unity files.

---
+++ Colors/Assets/Scripts/ArcadeScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class ArcadeScript : MonoBehaviour {
+	public Image background;
+	public Button yes;
+	public Button no;
+	public Text TimeText;
+	public Text ScoreText;
+	public List<Sprite> colorSprites = new List<Sprite>();
+	public float timeLeft = 100;
+	// Use this for initialization
+	void Start () {
+
+		
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		if(TimeText != null)
+		{
+			TimeText.text = "" + timeLeft.ToString("F2");
+		}
+		if (timeLeft <= 0)
+		{
+			GameOver();
+		}
+	}
+
+	private void GameOver()
+	{
+		Debug.Log("Game Over! Score is: " + UIManager.arcadeScore);
+		//SceneManager.LoadScene("GameOver");
+	}
+
+
+
+}
---
+++ Colors/Assets/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour {
+	public static int arcadeScore;
+	public Button launchArcadeBtnRef;
+	// Use this for initialization
+	void Start () {
+		if(launchArcadeBtnRef != null)
+		{
+			Button launchArcadeBtn = launchArcadeBtnRef.GetComponent<Button>();
+			launchArcadeBtn.onClick.AddListener(launchArcade);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void launchArcade()
+	{
+		//Opens Arcade Menu
+		SceneManager.LoadScene("ArcadeLevel");
+	}
+}
